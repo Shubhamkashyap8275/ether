1a5b218c098ccf463608bd0d0cd12981af807c05
shubham <shubham8275@gmail.com>
2023-08-18 21:24:00 +0530

    add connect button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Profile} from './profile'
+import {  WagmiConfig, createConfig, configureChains, mainnet  } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()],
+)
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig config={config}>
+    <Profile />
+  </WagmiConfig>
   );
 }
 
